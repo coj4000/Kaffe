936bd8abba8c676ce13f3c45230f2210687381f8
Christian <Christian@WORKSTATION>
2017-08-30 09:54:31 +0200

    Tilføjet Latte og KortadoKaffe

---
+++ Kaffe/CortadoKaffe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe
+{
+    class CortadoKaffe : Kaffe
+    {
+        public override int pris()
+        {
+            return 25;
+        }
+    }
+}
---
+++ Kaffe/Latte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe
+{
+    class Latte : Kaffe
+    {
+        public override int pris()
+        {
+            return 40;
+        }
+    }
+}
